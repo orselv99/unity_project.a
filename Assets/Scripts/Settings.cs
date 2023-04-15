00f31520b6a086f722a93b1cd3b1aea0be53b127
orseLv99 <orselv99@gmail.com>
2023-04-15 18:33:04 +0900

    settins volume slider 값에 따라 색상변경

--- Assets/Scripts/Settings.cs
+++ Assets/Scripts/Settings.cs
@@ -2,6 +2,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
+using static UnityEngine.Rendering.DebugUI;
 
 // 동적 스크롤바 생성
 // https://www.youtube.com/watch?v=X3LsvOvMpRU
@@ -24,6 +25,12 @@ public class Settings : MonoBehaviour
     private static string BACKGROUND_VOLUME = "BackgroundVolume";
     private static string EFFECT_VOLUME = "EffectVolume";
 
+    private readonly Color H0_VALUE = new Color(144f / 255f, 61f / 255f, 98f / 255f);
+    private readonly Color H1_VALUE = new Color(189f / 255f, 81f / 255f, 90f / 255f);
+    private readonly Color H2_VALUE = new Color(197f / 255f, 104f / 255f, 118f / 255f);
+    private readonly Color H3_VALUE = new Color(214f / 255f, 155f / 255f, 78f / 255f);
+    private readonly Color H4_VALUE = new Color(243f / 255f, 207f / 255f, 64f / 255f);
+
     [SerializeField]
     private Dropdown resolution = null;
     [SerializeField]
@@ -32,6 +39,8 @@ public class Settings : MonoBehaviour
     private Toggle invertedAim = null;
     [SerializeField]
     private Slider[] volumeSliders = null;
+    [SerializeField]
+    private Image[] volumeSliderFills = null;
 
     // gamemanager 에서 호출됨
     // 저장된 설정이 없으면 fallback 생성
@@ -99,6 +108,30 @@ public class Settings : MonoBehaviour
     }
 
     // ui events
+    private void ChangeFillColor(int index)
+    {
+        var value = volumeSliders[index].value;
+        if (value > 0.8f)
+        {
+            volumeSliderFills[index].color = H0_VALUE;
+        }
+        else if (value > 0.6f && value <= 0.8f)
+        {
+            volumeSliderFills[index].color = H1_VALUE;
+        }
+        else if (value > 0.4f && value <= 0.6f)
+        {
+            volumeSliderFills[index].color = H2_VALUE;
+        }
+        else if (value > 0.2f && value <= 0.4f)
+        {
+            volumeSliderFills[index].color = H3_VALUE;
+        }
+        else if (value <= 0.2f)
+        {
+            volumeSliderFills[index].color = H4_VALUE;
+        }
+    }
     public void OnValueChangedResolution(Dropdown dropdown)
     {
         Debug.Log(string.Format("Index: {0} {1}", dropdown.value, dropdown.options[dropdown.value].text));
@@ -122,6 +155,7 @@ public class Settings : MonoBehaviour
         GameManager.instance.setting.masterVolume = value;
         VolumeManager.instance.SetMasterVolume(value);
         PlayerPrefs.SetFloat(MASTER_VOLUME, value);
+        ChangeFillColor(0);
     }
     public void OnValueChangedBackgroundVolume(Slider slider)
     {
@@ -129,6 +163,7 @@ public class Settings : MonoBehaviour
         GameManager.instance.setting.backgroundVolume = value;
         VolumeManager.instance.SetBackgroundVolume(value);
         PlayerPrefs.SetFloat(BACKGROUND_VOLUME, value);
+        ChangeFillColor(1);
     }
     public void OnValueChangedEffectVolume(Slider slider)
     {
@@ -136,6 +171,7 @@ public class Settings : MonoBehaviour
         GameManager.instance.setting.effectVolume = value;
         VolumeManager.instance.SetEffectVolume(value);
         PlayerPrefs.SetFloat(EFFECT_VOLUME, value);
+        ChangeFillColor(2);
     }
     public void OnClickBack()
     {
